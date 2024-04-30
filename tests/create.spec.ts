b34d5d743c5982f3548a4dafa06c6661ba775499
luizeduardotesta <luizsonecazzz@hotmail.com>
2024-04-30 13:32:11 -0300

    add cenarios de erro para a rota post em usuarios

--- tests/create.spec.ts
+++ tests/create.spec.ts
@@ -3,27 +3,81 @@ import { createUser, deleteUser } from './support/api';
 import { readFileSync } from 'fs';
 import { UserModel } from './models/commonModels';
 
-export let userId: string
+export let userId: string;
 
 test.describe('Criar', () => {
-    test.only('Criar um usuário com sussesso', async ({ request }) => {
+    test('Criar um usuário com sussesso', async ({ request }) => {
         const userData = JSON.parse(readFileSync('tests/models/create.json', 'utf-8'));
         const user: UserModel = userData.success;
 
-        const postDesponse = await createUser(request, user);
-        expect(postDesponse.status()).toEqual(201)
+        const postResponse = await createUser(request, user);
+        expect(postResponse.status()).toEqual(201);
 
-        const postResponseBody = await postDesponse.json()
+        const postResponseBody = await postResponse.json();
 
-        expect(postResponseBody.message).toEqual('Cadastro realizado com sucesso')
-        expect(postResponseBody._id).toBeTruthy
-        userId = postResponseBody._id
+        expect(postResponseBody.message).toEqual('Cadastro realizado com sucesso');
+        expect(postResponseBody._id).toBeTruthy;
+        userId = postResponseBody._id;
 
         const deleteResponse = await deleteUser(request, userId);
-        expect(deleteResponse.status()).toEqual(200)
+        expect(deleteResponse.status()).toEqual(200);
+        const deleteResponseBody = await deleteResponse.json();
+        expect(deleteResponseBody.message).toEqual('Registro excluído com sucesso');
+    });
 
-        const deleteResponseBody = await deleteResponse.json()
+    test('Criar um usuário com email repetido', async ({ request }) => {
+        const userData = JSON.parse(readFileSync('tests/models/create.json', 'utf-8'));
+        const user: UserModel = userData.duplicate;
 
-        expect(deleteResponseBody.message).toEqual('Registro excluído com sucesso')
+        const postResponse = await createUser(request, user);
+        expect(postResponse.status()).toEqual(201);
+        const postResponseBody = await postResponse.json();
+        userId = postResponseBody._id;
+
+        const duplicateResponse = await createUser(request, user);
+        expect(duplicateResponse.status()).toEqual(400);
+
+        const duplicateResponseBody = await duplicateResponse.json();
+
+        expect(duplicateResponseBody.message).toEqual('Este email já está sendo usado')
+
+        const deleteResponse = await deleteUser(request, userId);
+        expect(deleteResponse.status()).toEqual(200);
+    });
+
+    test('Criar um usuário com o nome em branco', async ({request}) => {
+        const userData = JSON.parse(readFileSync('tests/models/create.json', 'utf-8'));
+        const user: UserModel = userData.nomeRequired;
+
+        const postResponse = await createUser(request, user);
+        expect(postResponse.status()).toEqual(400);
+
+        const postResponseBody = await postResponse.json();
+
+        expect(postResponseBody.nome).toEqual('nome não pode ficar em branco');
     })
-})
+
+    test('Criar um usuário com o email em branco', async ({request}) => {
+        const userData = JSON.parse(readFileSync('tests/models/create.json', 'utf-8'));
+        const user: UserModel = userData.emailRequired;
+
+        const postResponse = await createUser(request, user);
+        expect(postResponse.status()).toEqual(400);
+
+        const postResponseBody = await postResponse.json();
+
+        expect(postResponseBody.email).toEqual('email não pode ficar em branco');
+    })
+
+    test.only('Criar um usuário com a senha em branco', async ({request}) => {
+        const userData = JSON.parse(readFileSync('tests/models/create.json', 'utf-8'));
+        const user: UserModel = userData.passwordRequired;
+
+        const postResponse = await createUser(request, user);
+        expect(postResponse.status()).toEqual(400);
+
+        const postResponseBody = await postResponse.json();
+
+        expect(postResponseBody.password).toEqual('password não pode ficar em branco');
+    })
+});
